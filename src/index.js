868a0c7aad6a44ea52644a9c9f1dc01d73909460
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-11-05 19:45:10 -0400

    cleanup more

--- src/index.js
+++ src/index.js
@@ -22,8 +22,3 @@ ReactDOM.render(
   </Provider>,
   rootElement
 );
-
-//Task List:
-//1. Add in all necessary components and library methods.
-//2. Create a store that includes thunk and logger middleware support.
-//3. Wrap the App component in a react-redux Provider element.
